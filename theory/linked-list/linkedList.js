d7cc96ec3f159509a235c2ca8ff9a0d92e5988e9
Leont1cov <chuchunovvakov@mail.ru>
2026-02-09 22:08:40 +0300

    add linkedList.js, write base classes

---
+++ theory/linked-list/linkedList.js
@@ -0,0 +1,47 @@
+class LinkedListNode {
+    // инициализируем элемент
+    constructor(value, next = null) {
+        this.value = value
+        this.next = next
+    }
+
+    // Метод возвращает приведенной к строке текущий элемент
+    toString() {
+        return this.value
+    }
+}
+
+class LinkedList {
+    // не указываем head и tail в аргументах конструктора т.к. в будущем они будут сами определяться, а не явно
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+
+    append(value) {
+        const newNode = new LinkedListNode(value)
+
+        // Если элементов нет (ни head, ни tail), то тогда наш новый элемент становится и head и tail (то есть он единственный)
+        if (!this.head || !this.tail) {
+            this.head = newNode
+            this.tail = newNode
+
+            return this
+        }
+
+        // Если элементы в списке уже были, то берем хвост списка и указываем следующий элемент newNode (tail всегда указывает на null)
+        this.tail.next = newNode //записали новый элемент
+
+        this.tail = newNode //последний элемент становится хвостом
+
+        return this
+    }
+}
+
+const list = new LinkedList()
+list.append("a").append("b").append("c")
+
+console.log(JSON.stringify(list))
+/*
+{"head":{"value":"a","next":{"value":"b","next":{"value":"c","next":null}}},"tail":{"value":"c","next":null}}
+*/
