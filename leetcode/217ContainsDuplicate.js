2bcdeb266571b5b509619030308e6051267eeb34
Leont1cov <chuchunovvakov@mail.ru>
2026-02-18 20:51:48 +0300

    add: second solution option task 217 "Contains Duplicate"

--- leetcode/217ContainsDuplicate.js
+++ leetcode/217ContainsDuplicate.js
@@ -1,3 +1,17 @@
 const containsDuplicate = function(nums) {
     return new Set(nums).size !== nums.length
 };
+
+//2 вариант решения
+
+const containsDuplicate2 = function(nums) {
+    let storage = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] in storage) {
+            return true
+        }
+        storage[nums[i]] = true
+    }
+    return false
+}
