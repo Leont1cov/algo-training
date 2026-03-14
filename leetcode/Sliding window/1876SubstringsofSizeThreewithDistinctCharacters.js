ca6a3b4c069785968e26a0230a2e90422af70916
Leont1cov <chuchunovvakov@mail.ru>
2026-03-14 22:19:04 +0300

    decided: task 1876 "Substrings of Size Three with Distinct Characters"

---
+++ leetcode/Sliding window/1876SubstringsofSizeThreewithDistinctCharacters.js
@@ -0,0 +1,12 @@
+const countGoodSubstrings = function(s) {
+    let count = 0;
+
+    for (let i = 0; i <= s.length - 3; i++) {
+        let a = s[i], b = s[i + 1], c = s[i + 2];
+
+        if (a !== b && a !== c && b !== c) {
+            count++
+        }
+    }
+    return count;
+};
